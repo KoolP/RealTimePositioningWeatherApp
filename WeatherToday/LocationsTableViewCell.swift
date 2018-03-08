e02a8992be0f7eae69dc8c80ba6197df29588030
KoolP <patrik.rikama@gmail.com>
2018-03-08 23:25:03 +0100

    First commit

---
+++ WeatherToday/LocationsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationsTableViewCell.swift
+//  WeatherToday
+//
+//  Created by Patrik Rikama Hinnenberg on 2018-03-08.
+//  Copyright © 2018 Patrik Rikama Hinnenberg. All rights reserved.
+//
+
+import UIKit
+
+class LocationsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherToday/WeekWeatherForecastView.swift
@@ -0,0 +1,21 @@
+//
+//  WeekWeatherForecast.swift
+//  WeatherToday
+//
+//  Created by Patrik Rikama Hinnenberg on 2018-03-08.
+//  Copyright © 2018 Patrik Rikama Hinnenberg. All rights reserved.
+//
+
+import UIKit
+
+class WeekWeatherForecastView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
