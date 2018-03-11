4895e46ad14a71fb18e1e4e60d6ba89eb1f3e5b7
KoolP <patrik.rikama@gmail.com>
2018-03-11 19:40:41 +0100

    Cell files

---
+++ WeatherToday/ForecastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ForecastTableViewCell.swift
+//  WeatherToday
+//
+//  Created by Patrik Rikama Hinnenberg on 2018-03-11.
+//  Copyright © 2018 Patrik Rikama Hinnenberg. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
