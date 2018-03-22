5ddda4dc6d500cb599eda7af211e892876c4a75e
KoolP <patrik.rikama@gmail.com>
2018-03-23 00:04:15 +0100

    Started with model files

---
+++ WeatherToday/Models/MainDataManager.swift
@@ -0,0 +1,21 @@
+//
+//  MainDataManager.swift
+//  WeatherToday
+//
+//  Created by Patrik Rikama Hinnenberg on 2018-03-22.
+//  Copyright © 2018 Patrik Rikama Hinnenberg. All rights reserved.
+//
+//  This file communicates with the service and manipulating the data
+
+import Foundation
+
+class MainDataManager {
+//    //takes data from a immaginary local xml .plist
+//    - fileprivate func loadData() [[String: AnyObject]] {
+//    guard let path = Bundle.main.path(forResource: "MainCurrentData", ofType: "plist"),
+//    let items = NSArray(contentsOfFile: path) else {
+//    return [[:]]
+//    }
+//    }
+    
+}
